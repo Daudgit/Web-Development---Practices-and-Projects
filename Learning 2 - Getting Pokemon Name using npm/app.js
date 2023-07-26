628ea2bccbafc44e78810e6b890cf1bf1397be01
SK DAUD HASSAN <82196466+Daudgit@users.noreply.github.com>
2023-07-26 10:05:55 +0530

    Add files via upload

---
+++ Learning 2 - Getting Pokemon Name using npm/app.js
@@ -0,0 +1,7 @@
+const pokemon = require('pokemon');
+
+const randomPokemon = pokemon.random();
+console.log('Random Pokémon:', randomPokemon);
+
+const allPokemonNames = pokemon.all();
+console.log('All Pokémon:', allPokemonNames);
