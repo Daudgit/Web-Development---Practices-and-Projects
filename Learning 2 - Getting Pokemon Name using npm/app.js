f032b858e3bd9c9f1e8c5e628de510d55004922a
SK DAUD HASSAN <82196466+Daudgit@users.noreply.github.com>
2023-07-26 10:06:53 +0530

    Delete Learning 2 - Getting Pokemon Name using npm directory

--- Learning 2 - Getting Pokemon Name using npm/app.js
+++
@@ -1,7 +0,0 @@
-const pokemon = require('pokemon');
-
-const randomPokemon = pokemon.random();
-console.log('Random Pokémon:', randomPokemon);
-
-const allPokemonNames = pokemon.all();
-console.log('All Pokémon:', allPokemonNames);
